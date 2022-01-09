30fdd9ae50808dfc244a34fac56ddf890fdfc158
milesmaroney <miles@milesmaroney.com>
2022-01-08 21:34:38 -0800

    Demo messages

---
+++ src/demoMessages.js
@@ -0,0 +1,50 @@
+const moment = require('moment');
+
+const demoData = {
+  usernames: [
+    'seliM',
+    'Jax',
+    'prosurfer93',
+    'reactFan',
+    'notMiles',
+    'Jett',
+    'engineer37',
+    'UNUMspokesperson',
+    'theBoys',
+    'xDGuild',
+    'chikfila',
+  ],
+  colors: [
+    'rgb(255, 0, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 0, 255)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 255, 255)',
+    'rgb(0, 110, 255)',
+    'rgb(154, 0, 181)',
+    'rgb(181, 0, 94)',
+    'rgb(0, 181, 42)',
+  ],
+  timestamp: moment().format(),
+  messages: [
+    'i love this dark mode!',
+    'hey chatters',
+    'hows your day going',
+    'can anyone read this',
+    'nice LUL',
+    'the auto scroll pausing is so cool',
+    'its pretty wild that a couple months ago miles had never coded before',
+    'this is better than twitch LUL',
+    'needs more emotes!!! KEKW',
+    "you're amazing",
+    "you're beautiful",
+    'thanks for checking out my demo :)',
+    'have you tried dark mode?',
+    'have you tried colorblind mode?',
+    'have you tried enabling timestamps?',
+    'the smooth scrolling on new messages is nice',
+  ],
+};
+
+module.exports = demoData;
--- src/socket.js
+++ src/socket.js
@@ -4,6 +4,9 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const Options = require('./config');
+const demoData = require('./demoMessages');
+const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
 
 const PORT = Options.host === 'http://localhost' ? ':3000' : '';
 
@@ -27,6 +30,18 @@ io.on('connection', (socket) => {
     io.emit('message', msg);
   });
 });
+
+setInterval(() => {
+  io.emit('message', {
+    id: uuidv4(),
+    username:
+      demoData.usernames[Math.floor(Math.random() * demoData.usernames.length)],
+    color: demoData.colors[Math.floor(Math.random() * demoData.colors.length)],
+    timestamp: moment().format(),
+    message:
+      demoData.messages[Math.floor(Math.random() * demoData.messages.length)],
+  });
+}, 8000);
 
 server.listen(3001, () => {
   console.log(`listening on ${Options.host}:3001`);
